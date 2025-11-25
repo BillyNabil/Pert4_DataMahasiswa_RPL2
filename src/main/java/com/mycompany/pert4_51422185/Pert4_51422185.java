e6a01ea4d1ae0b46f9af2e7f45d84cb5d3587a11
NABIL AL FAROS <billynabil.nb123@gmail.com>
2025-11-25 16:25:32 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert4_51422185/Pert4_51422185.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_51422185;
+
+/**
+ *
+ * @author billy
+ */
+public class Pert4_51422185 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
